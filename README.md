1163ac9c910befa27277a1f76504e498bdf1cd5b
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-10-11 18:36:39 -0600

    fix delete bug & README

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Note Taker Starter Code
+# Note-Taker #
+
+Note-Taker an application that allows you to write and save notes with a title and plain text description, and delete notes as they become irrelevant.
+
+## Creation ## 
+
+### Created  Using ###
+*HTML
+*CSS
+*JavaScript
+  - Node.js
+    - Express.js
+
+This application uses an Express.js back end and saves and retrieves note data from a JSON file.
+
+It was my responsibility to build this back end and then connect it to a pre-existing front end. 
+
+
+## Live Application ##
+
+https://sfoutz-note-taker.herokuapp.com/
+
+## Demonstration ##
+
+![Note Taker](https://user-images.githubusercontent.com/68661461/95694515-a8ae0c00-0bef-11eb-8f21-b6df0a052333.gif)
+
+## Instructions ##
+
+*From the landing page, click the 'Get Started' button to navigate to the notes page. 
+*A blank note template is automatically displayed, allowing you to click and input a title, as well as a note description, then click the 'save' icon on the right side of the navigation bar. 
+*You will also see any previously saved notes listed on the left side and can click to enlarge any specific note for easy viewing. From here, to add a new note simply click the 'write' icon in the top right corner of the navigation bar.
+
+There you have it! As simply as that you are able to keep and re-visit a list of notes. 
+
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -19,8 +18,8 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('api/notes/:id', (req, res) => {
-  // let savedNotes = JSON.parse(fs.readFileSync(""))
-  res.json(savedNotes[Number(req.params.id)]);
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(notes[req.params.id]);
 });
 
 app.get('/', (req, res) => {
@@ -28,23 +27,22 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  // let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', "utf8"));
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   let newNote = req.body;
-  let noteId = (savedNotes.length + 1).toString();
+  let noteId = (notes.length + 1).toString();
   newNote.id = noteId;
-  savedNotes.push(newNote);
+  notes.push(newNote);
 
-  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes, null, 2));
-  console.log('Note saved to db.json. Content ', newNote);
-  res.json(savedNotes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2));
+  res.json(notes);
 });
 
 app.delete("/api/notes/:id", function(req, res) {
-  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   let noteID = req.params.id;
   let newID = 0;
   
-  savedNotes = savedNotes.filter(currNote => {
+  savedNotes = notes.filter(currNote => {
       return currNote.id != noteID;
   })
   
@@ -53,9 +51,9 @@ app.delete("/api/notes/:id", function(req, res) {
       newID++;
   }
 
-  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes, null, 2));
   res.json(savedNotes);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
